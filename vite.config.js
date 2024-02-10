194156f54638b5e6e66ccc796a7e57ba94233842
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 14:07:14 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    '/':'https://paperbrock-backend-beta.vercel.app/api/v1/beta/register'
+  }
 })
