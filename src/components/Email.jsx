23627ca0da09e8af3cb4b12469ac327afc95b7e0
parshvJS <parshvsheth2006@gmail.com>
2024-02-10 11:38:25 +0530

    cors error fixing

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -15,7 +15,7 @@ function Email() {
                 },
                 body: JSON.stringify({ email: emailAddress })
             };
-            await fetch('/api/v1/beta/register', options);
+            await fetch('https://paperbrock-backend-beta.vercel.app/api/v1/beta/register', options);
         } catch (error) {
             console.error(error);
         } finally {
